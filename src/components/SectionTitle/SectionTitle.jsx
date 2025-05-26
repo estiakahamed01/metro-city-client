7c6a79a990ec0a911680bd6ab1ce8909664b92e2
estiakahamed01 <tasin220116@diit.edu.bd>
2025-05-26 09:11:37 +0600

    Added Shared Title

---
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -0,0 +1,16 @@
+const SectionTitle = ({liteHeading,heading}) => {
+    return (
+        <div class="flex items-center justify-center my-8">
+        <div class="flex-grow border-t border-blue-900 border-2 relative top-0.5 mr-2"></div>
+        <div className="relative  flex flex-col justify-center items-center">
+          <h2 class="text-7xl font-semibold text-slate-200 uppercase">
+            {liteHeading}<span class="text-teal-900 text-lg"></span>
+          </h2>
+          <h2 className="absolute text-4xl left-44 right-[50%] font-bold top -[50%]">{heading}</h2>
+        </div>
+        <div class="flex-grow border-t border-blue-900 border-2 relative top-0.5 ml-2"></div>
+      </div>
+    );
+};
+
+export default SectionTitle;
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -4,13 +4,6 @@ const Home = () => {
   return (
     <div>
       <Banner></Banner>
-      {/* <div class="flex items-center justify-center my-8">
-        <div class="flex-grow border-t border-blue-900 relative top-0.5 mr-2"></div>
-        <h2 class="text-5xl font-semibold text-teal-900 relative">
-          Environment <span class="text-teal-900 text-lg">&#8250;</span>
-        </h2>
-        <div class="flex-grow border-t border-blue-900 relative top-0.5 ml-2"></div>
-      </div> */}
     </div>
   );
 };
